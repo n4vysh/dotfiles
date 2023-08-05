110fcd37d9f063fb6f8a4310d1bf0b3761441e42
n4vysh <n4vysh@n4vysh.dev>
2023-08-05 23:07:43 +0900

    feat(nvim): add rainbow-delimiters.nvim

--- src/.config/nvim/lua/user/plugins/syntax.lua
+++ src/.config/nvim/lua/user/plugins/syntax.lua
@@ -162,4 +162,31 @@ return {
 			end, { desc = "Jump previous todo comment" })
 		end,
 	},
+	{
+		"HiPhish/rainbow-delimiters.nvim",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		config = function()
+			local rainbow_delimiters = require("rainbow-delimiters")
+
+			vim.g.rainbow_delimiters = {
+				strategy = {
+					[""] = rainbow_delimiters.strategy["global"],
+					vim = rainbow_delimiters.strategy["local"],
+				},
+				query = {
+					[""] = "rainbow-delimiters",
+					lua = "rainbow-blocks",
+				},
+				highlight = {
+					"RainbowDelimiterRed",
+					"RainbowDelimiterYellow",
+					"RainbowDelimiterBlue",
+					"RainbowDelimiterOrange",
+					"RainbowDelimiterGreen",
+					"RainbowDelimiterViolet",
+					"RainbowDelimiterCyan",
+				},
+			}
+		end,
+	},
 }
