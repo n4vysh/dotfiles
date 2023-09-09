6e4435ca172e8986b77bf8940d8a4dc0c1ca4190
n4vysh <n4vysh@n4vysh.dev>
2023-09-09 16:39:37 +0900

    feat(nvim): tweak install command of lazy.nvim

--- src/.config/nvim/init.lua
+++ src/.config/nvim/init.lua
@@ -9,8 +9,8 @@ if not vim.loop.fs_stat(lazypath) then
 		"git",
 		"clone",
 		"--filter=blob:none",
-		"--single-branch",
 		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
 		lazypath,
 	})
 end
