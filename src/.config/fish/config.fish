bef96ceede3d13c78fe4a02bfb6d02636f0d02f4
n4vysh <n4vysh@n4vysh.dev>
2023-09-17 22:38:04 +0900

    feat(fish): add abbreviations for fish

--- src/.config/fish/config.fish
+++ src/.config/fish/config.fish
@@ -112,6 +112,8 @@ if status --is-interactive
     __fish_abbr_add_extend gi git
     __fish_abbr_add_extend gib gibo
     __fish_abbr_add_extend gp gping
+    __fish_abbr_add_extend gq ghq
+    __fish_abbr_add_extend gqg ghq get
     __fish_abbr_add_extend gr grex
     __fish_abbr_add_extend gt gpg-tui --style colored
     __fish_abbr_add_extend h helm
