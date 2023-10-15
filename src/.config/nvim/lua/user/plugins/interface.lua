7392c5a1c2e6ba11fcc24360651d9c238f8ff384
n4vysh <n4vysh@n4vysh.dev>
2023-10-15 15:58:38 +0900

    feat(nvim): bump plugins

--- src/.config/nvim/lua/user/plugins/interface.lua
+++ src/.config/nvim/lua/user/plugins/interface.lua
@@ -25,6 +25,14 @@ return {
 	},
 	{
 		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {
+			indent = {
+				char = "│",
+				tab_char = "│",
+			},
+			scope = { enabled = false },
+		},
 		event = "VeryLazy",
 	},
 	{
@@ -69,14 +77,12 @@ return {
 			local builtin = require("statuscol.builtin")
 			require("statuscol").setup({
 				segments = {
+					{ text = { "%C" }, click = "v:lua.ScFa" },
+					{ text = { "%s" }, click = "v:lua.ScSa" },
 					{
-						sign = { name = { "Diagnostic" }, colwidth = 1 },
-						click = "v:lua.ScSa",
-					},
-					{ text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
-					{
-						sign = { name = { ".*" }, colwidth = 1 },
-						click = "v:lua.ScSa",
+						text = { builtin.lnumfunc, " " },
+						condition = { true, builtin.not_empty },
+						click = "v:lua.ScLa",
 					},
 				},
 			})
