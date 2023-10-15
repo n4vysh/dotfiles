e7155b1cc1d67ffa2b0cf66d162a18d5f40a8ea3
n4vysh <n4vysh@n4vysh.dev>
2023-10-15 16:13:48 +0900

    feat(nvim): add telescope custom action for multiple select

--- src/.config/nvim/lua/user/plugins/finder.lua
+++ src/.config/nvim/lua/user/plugins/finder.lua
@@ -161,6 +161,23 @@ return {
 			table.insert(vimgrep_arguments, "--glob")
 			table.insert(vimgrep_arguments, "!**/.git/*")
 
+			-- NOTE: Use custom action to select multiple files
+			-- https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-1679797700
+			local select_one_or_multi = function(prompt_bufnr)
+				local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+				local multi = picker:get_multi_selection()
+				if not vim.tbl_isempty(multi) then
+					require("telescope.actions").close(prompt_bufnr)
+					for _, j in pairs(multi) do
+						if j.path ~= nil then
+							vim.cmd(string.format("%s %s", "edit", j.path))
+						end
+					end
+				else
+					require("telescope.actions").select_default(prompt_bufnr)
+				end
+			end
+
 			telescope.setup({
 				defaults = require("telescope.themes").get_dropdown({
 					vimgrep_arguments = vimgrep_arguments,
@@ -169,6 +186,7 @@ return {
 							["<C-f>"] = false,
 							["<C-u>"] = false,
 							["<C-d>"] = false,
+							["<CR>"] = select_one_or_multi,
 						},
 					},
 					prompt_prefix = "▶ ",
