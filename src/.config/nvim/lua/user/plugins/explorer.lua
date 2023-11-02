b86b1ce16c2f231788eee0549e93d2494c76a62e
n4vysh <n4vysh@n4vysh.dev>
2023-11-03 08:51:54 +0900

    feat(nvim): update nvim-tree.lua for lazy load

--- src/.config/nvim/lua/user/plugins/explorer.lua
+++ src/.config/nvim/lua/user/plugins/explorer.lua
@@ -1,16 +1,18 @@
 return {
 	{
 		"nvim-tree/nvim-tree.lua",
-		cmd = { "NvimTreeToggle", "NvimTreeFindFile" },
-		init = function()
-			vim.keymap.set("n", "<Space>ft", function()
-				if vim.fn.expand("%") == "" then
-					vim.cmd.NvimTreeToggle()
-				else
-					vim.cmd.NvimTreeFindFile()
-				end
-			end)
-		end,
+		keys = {
+			{
+				"<Space>ft",
+				function()
+					if vim.fn.expand("%") == "" then
+						vim.cmd.NvimTreeToggle()
+					else
+						vim.cmd.NvimTreeFindFile()
+					end
+				end,
+			},
+		},
 		config = function()
 			require("nvim-tree").setup({
 				hijack_cursor = true,
