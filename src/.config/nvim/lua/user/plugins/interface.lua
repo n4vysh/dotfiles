315c575653bb998eb4cfbc40a0cba7370e00c18c
n4vysh <n4vysh@n4vysh.dev>
2023-11-03 08:55:12 +0900

    feat(nvim): tweak neovim-session-manager for lazy load

--- src/.config/nvim/lua/user/plugins/interface.lua
+++ src/.config/nvim/lua/user/plugins/interface.lua
@@ -189,11 +189,15 @@ return {
 			"nvim-tree/nvim-web-devicons",
 			{
 				"Shatur/neovim-session-manager",
+				cmd = { "SessionManager" },
 				config = function()
 					require("session_manager").setup({
 						autoload_mode = require("session_manager.config").AutoloadMode.Disabled,
 					})
 				end,
+				dependencies = {
+					"nvim-lua/plenary.nvim",
+				},
 			},
 		},
 	},
