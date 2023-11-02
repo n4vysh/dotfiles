927dfd904d9f79d33cca0653468d061395c8209d
n4vysh <n4vysh@n4vysh.dev>
2023-11-03 08:50:27 +0900

    feat(nvim): remove unnecessary option

--- src/.config/nvim/lua/user/plugins/browser.lua
+++ src/.config/nvim/lua/user/plugins/browser.lua
@@ -5,9 +5,6 @@ return {
 		keys = {
 			{ "gx", "<Plug>(openbrowser-smart-search)", mode = { "n", "v" }, silent = true },
 		},
-		init = function()
-			vim.g.netrw_nogx = 1
-		end,
 	},
 	{
 		"axieax/urlview.nvim",
