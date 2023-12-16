a4a9d4375f11530f2ac85faa47e9318c3e86d509
n4vysh <n4vysh@n4vysh.dev>
2023-12-16 19:30:14 +0900

    feat(nvim): bump plugins

--- src/.config/nvim/lua/user/plugins/completion.lua
+++ src/.config/nvim/lua/user/plugins/completion.lua
@@ -49,6 +49,8 @@ return {
 			{ "onsails/lspkind.nvim" },
 			{
 				"L3MON4D3/LuaSnip",
+				version = "v2.*",
+				build = "make install_jsregexp",
 				config = function()
 					require("luasnip.loaders.from_vscode").lazy_load()
 
