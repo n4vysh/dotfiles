7ae35bf9534fc396d687a20ee4a0ca6d81c7804b
n4vysh <n4vysh@n4vysh.dev>
2024-01-05 17:22:47 +0900

    feat(waybar): tweak script for VPN

--- src/.config/waybar/lib/vpn.bash
+++ src/.config/waybar/lib/vpn.bash
@@ -5,7 +5,8 @@ result="$?"
 
 [[ "$result" == 1 ]] && echo '{"alt": "disconnected"}'
 
+# NOTE: `nordvpn status` print control character, so use backward match
 nordvpn status |
-	grep 'Status: Connected' >/dev/null &&
+	grep -E 'Status: Connected$' >/dev/null &&
 	echo '{"alt": "connected"}' ||
 	echo '{"alt": "disconnected"}'
