438ad139dec5e8eeb9bfcf2b3af9e083c3b3fb68
n4vysh <n4vysh@n4vysh.dev>
2024-05-26 15:13:59 +0900

    feat: remove unnecessary wrapper script for nnn

--- src/.local/src/dotfiles/imvr.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# for nnn
-# https://github.com/jarun/nnn/blob/35aa8645541f2a069949a81faba9f028726765ad/plugins/nuke#L314
-# related issue: https://github.com/jarun/nnn/pull/715
-imv "${@}"
