aea5866dac6f7dceb317e2fde2c6bf2f8464a3ef
n4vysh <n4vysh@n4vysh.dev>
2024-05-26 15:09:03 +0900

    feat(nvim): add keymap to close window for neotest

--- src/.config/nvim/lua/user/plugins/test.lua
+++ src/.config/nvim/lua/user/plugins/test.lua
@@ -75,6 +75,24 @@ return {
 					}),
 				},
 			})
+
+			do
+				local augroup = "neotest_close_mappings"
+				vim.api.nvim_create_augroup(augroup, { clear = true })
+				for _, ft in ipairs({ "output", "attach", "summary" }) do
+					vim.api.nvim_create_autocmd("FileType", {
+						group = augroup,
+						pattern = "neotest-" .. ft,
+						callback = function(opts)
+							vim.keymap.set("n", "q", function()
+								pcall(vim.api.nvim_win_close, 0, true)
+							end, {
+								buffer = opts.buf,
+							})
+						end,
+					})
+				end
+			end
 		end,
 		dependencies = {
 			"nvim-neotest/nvim-nio",
