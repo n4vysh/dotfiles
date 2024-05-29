51c663dbb80283b6267d684965a6b7071d61dfb3
n4vysh <n4vysh@n4vysh.dev>
2024-05-29 16:53:13 +0900

    feat(nvim): add ChatGPT.nvim

--- src/.config/nvim/lua/user/plugins/ai.lua
+++ src/.config/nvim/lua/user/plugins/ai.lua
@@ -17,4 +17,55 @@ return {
 			{ "zbirenbaum/copilot.lua" },
 		},
 	},
+	{
+		"jackMort/ChatGPT.nvim",
+		event = { "BufReadPost", "BufAdd", "BufNewFile" },
+		keys = {
+			{ "<space>ai", "<cmd>ChatGPTRun explain_code<cr>", desc = "Explain code with ChatGPT" },
+		},
+		opts = {
+			api_key_cmd = "gopass show -o n4vysh/openai/chatgpt.nvim",
+			chat = {
+				sessions_window = {
+					border = {
+						style = "single",
+					},
+				},
+			},
+			popup_window = {
+				border = {
+					style = "single",
+				},
+			},
+			system_window = {
+				border = {
+					style = "single",
+				},
+			},
+			popup_input = {
+				border = {
+					style = "single",
+				},
+			},
+			settings_window = {
+				border = {
+					style = "single",
+				},
+			},
+			help_window = {
+				border = {
+					style = "single",
+				},
+			},
+			actions_paths = {
+				vim.fn.stdpath("config") .. "/misc/chatgpt-actions.json",
+			},
+		},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"folke/trouble.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
 }
