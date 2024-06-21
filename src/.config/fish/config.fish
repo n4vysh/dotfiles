07df10b6bc61ebe21c1c21a56e154bfcd438f915
n4vysh <n4vysh@n4vysh.dev>
2024-06-21 16:30:59 +0900

    feat(fish): add abbreviation for kubectl version

--- src/.config/fish/config.fish
+++ src/.config/fish/config.fish
@@ -194,6 +194,7 @@ if status --is-interactive
         fzf +m \| \
         awk \'{print \$1}\' \| \
         xargs -t -I {} kubectl logs {}
+    __fish_abbr_add_extend kcv kubectl version
     __fish_abbr_add_extend kl kube-linter
     __fish_abbr_add_extend kn kubens
     __fish_abbr_add_extend kv kubeval
