f47e8cab437a964f19225657aad63fab6d1b8dbb
n4vysh <n4vysh@n4vysh.dev>
2024-09-01 20:39:00 +0900

    feat(nvim): add avante.nvim

--- src/.config/nvim/lua/user/plugins/ai.lua
+++ src/.config/nvim/lua/user/plugins/ai.lua
@@ -23,4 +23,64 @@ return {
 			{ "zbirenbaum/copilot.lua" },
 		},
 	},
+	{
+		"yetone/avante.nvim",
+		event = { "BufReadPost", "BufAdd", "BufNewFile" },
+		keys = {
+			{
+				"<space>ai",
+				function()
+					require("avante.api").ask()
+				end,
+				desc = "Show sidebar for AI",
+				mode = { "n", "v" },
+			},
+			{
+				"<space>ar",
+				function()
+					require("avante.api").refresh()
+				end,
+				desc = "Refresh sidebar for AI",
+				mode = { "n", "v" },
+			},
+			{
+				"<space>ae",
+				function()
+					require("avante.api").edit()
+				end,
+				desc = "Edit selected blocks for AI",
+				mode = { "v" },
+			},
+		},
+		opts = {
+			hints = { enabled = false },
+		},
+		build = ":AvanteBuild",
+		dependencies = {
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons",
+			{
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+					},
+				},
+			},
+			{
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "Avante" },
+				},
+				ft = { "Avante" },
+			},
+		},
+	},
 }
