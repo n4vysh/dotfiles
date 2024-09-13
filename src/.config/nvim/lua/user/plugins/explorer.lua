6f8c41082445cf979575c301c7093155e98cc17b
n4vysh <n4vysh@n4vysh.dev>
2024-09-13 23:23:14 +0900

    feat(nvim): add custom keymaps of nvim-tree.lua

--- src/.config/nvim/lua/user/plugins/explorer.lua
+++ src/.config/nvim/lua/user/plugins/explorer.lua
@@ -49,6 +49,62 @@ return {
 					},
 				},
 			},
+			on_attach = function(bufnr)
+				local api = require("nvim-tree.api")
+
+				local function opts(desc)
+					return {
+						desc = "nvim-tree: " .. desc,
+						buffer = bufnr,
+						noremap = true,
+						silent = true,
+						nowait = true,
+					}
+				end
+
+				-- default mappings
+				api.config.mappings.default_on_attach(bufnr)
+
+				-- custom mappings
+				vim.keymap.del("n", "g?", opts("Help"))
+				vim.keymap.set("n", "<C-_>", api.tree.toggle_help, opts("Help"))
+				vim.keymap.del("n", "ge", opts("Copy Basename"))
+				vim.keymap.del("n", "gy", opts("Copy Absolute Path"))
+				vim.keymap.del("n", "Y", opts("Copy Relative Path"))
+				vim.keymap.del("n", "y", opts("Copy Name"))
+				vim.keymap.set(
+					"n",
+					"yb",
+					api.fs.copy.basename,
+					opts("Copy Basename")
+				)
+				vim.keymap.set(
+					"n",
+					"yf",
+					api.fs.copy.absolute_path,
+					opts("Copy Absolute Path")
+				)
+				vim.keymap.set(
+					"n",
+					"yr",
+					api.fs.copy.relative_path,
+					opts("Copy Relative Path")
+				)
+				vim.keymap.set(
+					"n",
+					"yn",
+					api.fs.copy.filename,
+					opts("Copy Name")
+				)
+				vim.keymap.del("n", "e", opts("Rename: Basename"))
+
+				local function edit_and_close(node)
+					api.node.open.edit(node, { close_tree = true })
+					api.tree.close()
+				end
+
+				vim.keymap.set("n", "e", edit_and_close, opts("Edit"))
+			end,
 		},
 		dependencies = {
 			"stevearc/dressing.nvim",
