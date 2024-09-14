e82fbbc656841581bce68f60dd36e4068ab2f94c
n4vysh <n4vysh@n4vysh.dev>
2024-09-15 00:21:02 +0900

    feat(qmk): add voyager

---
+++ src/.config/qmk/keyboards/zsa/voyager/keymap/config.h
@@ -0,0 +1,35 @@
+/* Copyright 2024 n4vysh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define MATRIX_COLS 7
+#define MATRIX_ROWS 12
+
+#define IS31FL3731_I2C_ADDRESS_1 IS31FL3731_I2C_ADDRESS_GND
+#define IS31FL3731_I2C_ADDRESS_2 IS31FL3731_I2C_ADDRESS_VCC
+
+#define IS31FL3731_I2C_TIMEOUT 5
+
+#define MOUSEKEY_WHEEL_INTERVAL MOUSEKEY_INTERVAL
+#define MOUSEKEY_WHEEL_MAX_SPEED MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_WHEEL_TIME_TO_MAX MOUSEKEY_TIME_TO_MAX
+
+#define VOYAGER_USER_LEDS
+
+#define PERMISSIVE_HOLD
+
+#define MK_KINETIC_SPEED
