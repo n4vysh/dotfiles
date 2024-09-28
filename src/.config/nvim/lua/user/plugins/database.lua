83d34e623c2d316061d195c36afe3376d0802c3d
n4vysh <n4vysh@n4vysh.dev>
2024-09-29 02:20:28 +0900

    feat(nvim): enable nvim-notify in vim-dadbod-ui

--- src/.config/nvim/lua/user/plugins/database.lua
+++ src/.config/nvim/lua/user/plugins/database.lua
@@ -13,6 +13,7 @@ return {
 		},
 		init = function()
 			vim.g.db_ui_use_nerd_fonts = 1
+			vim.g.db_ui_use_nvim_notify = 1
 			vim.g.db_ui_save_location = "~/Documents/sql/"
 			vim.g.db_ui_table_helpers = {
 				postgres = {
@@ -54,6 +55,7 @@ return {
 			{ "tpope/vim-dadbod" },
 			{ "tpope/vim-dotenv" },
 			{ "kristijanhusak/vim-dadbod-completion" },
+			{ "rcarriga/nvim-notify" },
 		},
 	},
 }
