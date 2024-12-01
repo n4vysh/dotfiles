354b76650afd363f84ceb9c1dc809253d2957f32
n4vysh <n4vysh@n4vysh.dev>
2024-12-01 22:26:38 +0900

    feat(nvim): add smear-cursor.nvim

--- src/.config/nvim/lua/user/plugins/interface.lua
+++ src/.config/nvim/lua/user/plugins/interface.lua
@@ -520,6 +520,18 @@ return {
 		},
 	},
 	{
+		"sphamba/smear-cursor.nvim",
+		event = { "VimEnter" },
+		opts = {
+			stiffness = 0.8,
+			trailing_stiffness = 0.6,
+			trailing_exponent = 0,
+			distance_stop_animating = 0.5,
+			hide_target_hack = false,
+			legacy_computing_symbols_support = true,
+		},
+	},
+	{
 		"karb94/neoscroll.nvim",
 		event = { "VimEnter" },
 		opts = {},
