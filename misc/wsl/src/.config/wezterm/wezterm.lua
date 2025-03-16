b6bb06aa94dd560eebe93351de070ea63fe5bb39
n4vysh <n4vysh@n4vysh.dev>
2025-03-16 19:19:28 +0900

    revert: "chore: add wrapper script for WSL"
    This reverts commit 4a79a63fc314edec74b3b1c6d74205186fbc6476.

--- misc/wsl/src/.config/wezterm/wezterm.lua
+++ misc/wsl/src/.config/wezterm/wezterm.lua
@@ -13,9 +13,7 @@ return {
 	color_scheme = "tokyonight",
 	enable_tab_bar = false,
 	window_background_opacity = 0.9,
-	-- HACK: use wrapper script to avoid crash
-	--       WSL can't start tmux in .zshrc
-	default_prog = { "/Windows/System32/wsl.exe", "~", "-d", "Arch", "tmuxs" },
+	default_domain = "WSL:Arch",
 	canonicalize_pasted_newlines = "LineFeed",
 	adjust_window_size_when_changing_font_size = false,
 	allow_win32_input_mode = false,
