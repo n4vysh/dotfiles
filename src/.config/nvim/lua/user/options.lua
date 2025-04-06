685a3596c122dc5bc96ac23b395d610cbd78b81e
n4vysh <n4vysh@n4vysh.dev>
2025-04-06 20:32:09 +0900

    feat(nvim): use vim.diagnostic.config instead of vim.fn.sign_define

--- src/.config/nvim/lua/user/options.lua
+++ src/.config/nvim/lua/user/options.lua
@@ -78,11 +78,16 @@ if vim.env.DISPLAY ~= "" then
 	vim.opt.clipboard = "unnamedplus"
 end
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.HINT] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+		},
+	},
+})
 
 vim.filetype.add({
 	extension = {
