1667dc944128c18358504a7914d862f1799e270a
n4vysh <n4vysh@n4vysh.dev>
2025-04-20 04:49:20 +0900

    refactor(nvim): :recycle: add startup.lua

--- src/.config/nvim/init.lua
+++ src/.config/nvim/init.lua
@@ -72,37 +72,10 @@ lazy.setup("user.plugins", {
 	},
 })
 
+require("user.startup")
 require("user.options")
 require("user.autocmds")
 require("user.keymaps")
 if pcall(require, "user.local") then
 	require("user.local")
-end
-
--- NOTE: remove directory buffer when startup
-for _, v in ipairs(vim.fn.argv()) do
-	if vim.fn.isdirectory(v) ~= 0 then
-		local bufnr = vim.fn.bufnr(v)
-		if bufnr ~= -1 then
-			local success, result = pcall(function()
-				vim.api.nvim_buf_delete(bufnr, { force = true })
-			end)
-			if success then
-				vim.notify(
-					"Deleted directory from buffer list: "
-						.. vim.fn.fnamemodify(v, ":.")
-						.. "\n",
-					vim.log.levels.WARN
-				)
-			else
-				vim.notify(
-					"Failed to delete buffer for: "
-						.. v
-						.. ". Error: "
-						.. tostring(result),
-					vim.log.levels.ERROR
-				)
-			end
-		end
-	end
 end
