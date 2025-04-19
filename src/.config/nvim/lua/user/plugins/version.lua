acb4a574d3f38cb6fa76c386e7fa26e8ecf92fc4
n4vysh <n4vysh@n4vysh.dev>
2025-04-20 04:50:21 +0900

    feat(nvim): :sparkles: add lazygit.nvim

--- src/.config/nvim/lua/user/plugins/version.lua
+++ src/.config/nvim/lua/user/plugins/version.lua
@@ -135,4 +135,22 @@ return {
 			},
 		},
 	},
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		keys = {
+			{ "<space>vv", "<cmd>LazyGit<cr>", desc = "Open lazygit" },
+		},
+		init = function()
+			vim.g.lazygit_floating_window_border_chars =
+				{ "┌", "─", "┐", "│", "┘", "─", "└", "│" }
+		end,
+	},
 }
