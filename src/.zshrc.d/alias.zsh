29b19744b35fdae6c6dc72286f7c023473d55b75
n4vysh <n4vysh@n4vysh.dev>
2025-04-28 17:18:14 +0900

    docs(zsh): :memo: add comment explaining hyperlink feature in alias

--- src/.zshrc.d/alias.zsh
+++ src/.zshrc.d/alias.zsh
@@ -2,6 +2,7 @@ alias ip='ip -c'
 alias cp='cp -iv'
 alias mv='mv -iv'
 alias rm='rm -iv'
+# enable OSC 8 with --hyperlink option
 alias ls='ls --hyperlink'
 alias eza='eza -gF --icons --git --time-style long-iso -s Name --hyperlink'
 alias kubectl=kubecolor
