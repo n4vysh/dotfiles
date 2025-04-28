2f784f93b90076b86f174824ad8805c9c140673b
n4vysh <n4vysh@n4vysh.dev>
2025-04-29 04:33:06 +0900

    feat(zsh): :sparkles: add fd and ripgrep aliases for hyperlink support

--- src/.zshrc.d/alias.zsh
+++ src/.zshrc.d/alias.zsh
@@ -5,4 +5,6 @@ alias rm='rm -iv'
 # enable OSC 8 with --hyperlink option
 alias ls='ls --hyperlink'
 alias eza='eza -gF --icons --git --time-style long-iso -s Name --hyperlink'
+alias fd='fd --hyperlink'
+alias rg='rg --hyperlink-format default'
 alias kubectl=kubecolor
