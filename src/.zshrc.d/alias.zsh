5dafcae073b4428d883921a1e078e6c6af3bb9fe
n4vysh <n4vysh@n4vysh.dev>
2025-04-28 12:15:44 +0900

    feat(zsh): :sparkles: add OSC 8 support to ls and eza commands

--- src/.zshrc.d/alias.zsh
+++ src/.zshrc.d/alias.zsh
@@ -2,5 +2,6 @@ alias ip="ip -c"
 alias cp="cp -iv"
 alias mv="mv -iv"
 alias rm="rm -iv"
-alias eza='eza -gF --icons --git --time-style long-iso -s Name'
+alias ls='ls --hyperlink'
+alias eza='eza -gF --icons --git --time-style long-iso -s Name --hyperlink'
 alias kubectl=kubecolor
