84540fcbd84152f779622592b4da51e9d0da7c0b
n4vysh <n4vysh@n4vysh.dev>
2025-04-28 12:17:28 +0900

    style(zsh): :lipstick: change double quotes to single quotes in alias definitions for consistency

--- src/.zshrc.d/alias.zsh
+++ src/.zshrc.d/alias.zsh
@@ -1,7 +1,7 @@
-alias ip="ip -c"
-alias cp="cp -iv"
-alias mv="mv -iv"
-alias rm="rm -iv"
+alias ip='ip -c'
+alias cp='cp -iv'
+alias mv='mv -iv'
+alias rm='rm -iv'
 alias ls='ls --hyperlink'
 alias eza='eza -gF --icons --git --time-style long-iso -s Name --hyperlink'
 alias kubectl=kubecolor
