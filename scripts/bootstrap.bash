2aa7dcb13fb6261a8cf044ee678ac57c618ac89f
n4vysh <n4vysh@n4vysh.dev>
2025-05-05 01:57:22 +0900

    feat(pacman): :sparkles: add makepkg.conf

--- scripts/bootstrap.bash
+++ scripts/bootstrap.bash
@@ -392,16 +392,6 @@ _configure_without_privileged() {
 			tr '\n' ' '
 	)"
 
-	_log::info 'Configure makepkg'
-	sudo sed \
-		-i \
-		-e 's/-march=x86-64 -mtune=generic/-march=native/' \
-		-e "/^#MAKEFLAGS=\"-j2\"$/s/-j2/-j$(nproc)/" \
-		-e "s/^#MAKEFLAGS/MAKEFLAGS/" \
-		-e '/^COMPRESSZST/s/zstd -c/zstd -1 --threads=0 -c/' \
-		-e '/^BUILDENV/s/!ccache/ccache/' \
-		/etc/makepkg.conf
-
 	_log::info 'Configure pkgfile'
 	sudo systemctl start pkgfile-update.service
 	sudo systemctl enable --now pkgfile-update.timer
