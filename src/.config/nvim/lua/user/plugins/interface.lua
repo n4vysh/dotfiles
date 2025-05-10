9f03c38adf07b48fa0859be70584c3c00c7447f6
n4vysh <n4vysh@n4vysh.dev>
2025-05-10 23:56:42 +0900

    feat(nvim): :sparkles: add context menus

--- src/.config/nvim/lua/user/plugins/interface.lua
+++ src/.config/nvim/lua/user/plugins/interface.lua
@@ -974,4 +974,33 @@ return {
 			"smjonas/inc-rename.nvim",
 		},
 	},
+	{ "nvzone/volt", lazy = true },
+	{
+		"nvzone/menu",
+		lazy = true,
+		keys = {
+			{
+				"<C-m>",
+				function()
+					require("menu").open("default")
+				end,
+			},
+			{
+				"<RightMouse>",
+				function()
+					require("menu.utils").delete_old_menus()
+
+					vim.cmd.exec('"normal! \\<RightMouse>"')
+
+					local buf =
+						vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+					local options = vim.bo[buf].ft == "NvimTree" and "nvimtree"
+						or "default"
+
+					require("menu").open(options, { mouse = true })
+				end,
+				mode = { "n", "v" },
+			},
+		},
+	},
 }
