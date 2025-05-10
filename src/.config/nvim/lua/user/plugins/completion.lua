0b34ca634ada1f223ef20b1a2ec0b598208879d8
n4vysh <n4vysh@n4vysh.dev>
2025-05-11 02:12:10 +0900

    feat(nvim): :sparkles: add border to nvim-cmp

--- src/.config/nvim/lua/user/plugins/completion.lua
+++ src/.config/nvim/lua/user/plugins/completion.lua
@@ -30,6 +30,14 @@ return {
 						require("luasnip").lsp_expand(args.body)
 					end,
 				},
+				window = {
+					completion = cmp.config.window.bordered({
+						border = "single",
+					}),
+					documentation = cmp.config.window.bordered({
+						border = "single",
+					}),
+				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
