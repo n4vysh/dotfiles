2463c67b7ed226de6e1e25d48fb7b5e88a86c799
n4vysh <n4vysh@n4vysh.dev>
2025-06-01 23:05:42 +0900

    feat(home-manager): :sparkles: use 25.05 release branch of stable channel

--- src/.config/home-manager/flake.nix
+++ src/.config/home-manager/flake.nix
@@ -2,9 +2,9 @@
   description = "Home Manager configuration of n4vysh";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland-contrib = {
