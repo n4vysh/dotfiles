815e4d60e1c6826c3075d27e5213b3755e6d4252
n4vysh <n4vysh@n4vysh.dev>
2025-07-13 23:57:30 +0900

    chore: :hammer: add security tools to pkglist

--- src/.zshrc.d/conf.d/alias.zsh
+++ src/.zshrc.d/conf.d/alias.zsh
@@ -2,6 +2,7 @@ alias ip='ip -c'
 alias cp='cp -iv'
 alias mv='mv -iv'
 alias rm='rm -iv'
+alias httpx=httpx-pd
 # enable OSC 8 with --hyperlink option
 alias ls='ls --hyperlink'
 alias eza='eza -gF --icons --git --time-style long-iso -s Name --hyperlink'
