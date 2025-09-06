995d7e3aa133a4b49655efc8472bb369279a2d16
n4vysh <n4vysh@n4vysh.dev>
2025-09-06 12:06:29 +0900

    feat(zsh): :sparkles: add ssh alias to force xterm-256color

--- src/dot_zshrc.d/conf.d/alias.zsh
+++ src/dot_zshrc.d/conf.d/alias.zsh
@@ -3,6 +3,7 @@ alias cp='cp -iv'
 alias mv='mv -iv'
 alias rm='rm -iv'
 alias httpx=httpx-pd
+alias ssh='TERM=xterm-256color ssh'
 # enable OSC 8 with --hyperlink option
 alias ls='ls --hyperlink'
 alias eza='eza -gF --icons --git --time-style long-iso -s Name --hyperlink'
