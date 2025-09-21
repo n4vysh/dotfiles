60915fdc93a01c6fcfc202f47046aa10bbe0b9b9
n4vysh <n4vysh@n4vysh.dev>
2025-09-21 21:56:39 +0900

    feat(nvim): :sparkles: set chezmoi source directory path to ~/.local/share/chezmoi/src

--- src/dot_config/nvim/lua/user/plugins/filetype.lua
+++ src/dot_config/nvim/lua/user/plugins/filetype.lua
@@ -65,6 +65,8 @@ return {
 		lazy = false,
 		init = function()
 			vim.g["chezmoi#use_tmp_buffer"] = true
+			vim.g["chezmoi#source_dir_path"] = vim.fn.expand("~")
+				.. "/.local/share/chezmoi/src"
 
 			do
 				local augroup = "chezmoi"
