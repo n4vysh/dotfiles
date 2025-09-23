6e99a690a5e219873c9fc90284acd5e257e5db4f
n4vysh <n4vysh@n4vysh.dev>
2025-09-23 20:05:01 +0900

    chore: :hammer: change script for WSL to copy systemd and sudoers config files

---
+++ home/.chezmoiscripts/linux/wsl/run_onchange_after_101_enable-services-for-wsl.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eu
+
+WSL_DISTRO_NAME="${WSL_DISTRO_NAME:=none}"
+
+if [ "$WSL_DISTRO_NAME" = "none" ]; then
+	gum log --level warn "$0: not running on WSL -- skipping"
+	exit 0
+fi
+
+gum log --level info 'Enable systemd services'
+systemctl --user daemon-reload
+# NOTE: fix wslg bug of WSL
+# https://github.com/microsoft/wslg/issues/1032#issuecomment-2310369848
+systemctl --user enable --now wsl-wayland-symlink.service
