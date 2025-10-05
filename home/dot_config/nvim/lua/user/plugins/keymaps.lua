a857ab7e718c1888f3767728a28270d4473928e4
n4vysh <n4vysh@n4vysh.dev>
2025-10-05 20:59:51 +0900

    feat(nvim): :sparkles: remove mini.bracketed

--- home/dot_config/nvim/lua/user/plugins/keymaps.lua
+++ home/dot_config/nvim/lua/user/plugins/keymaps.lua
@@ -12,59 +12,7 @@ return {
 			vim.cmd("silent! xunmap ]uU")
 			vim.cmd("silent! nunmap [uU")
 			vim.cmd("silent! nunmap ]uU")
-			-- remove keymap for mini.bracketed
-			vim.cmd("silent! xunmap [n")
-			vim.cmd("silent! xunmap ]n")
-			vim.cmd("silent! nunmap [n")
-			vim.cmd("silent! nunmap ]n")
-			vim.cmd("silent! xunmap [x")
-			vim.cmd("silent! xunmap ]x")
-			vim.cmd("silent! nunmap [x")
-			vim.cmd("silent! nunmap ]x")
-			vim.cmd("silent! nunmap [xx")
-			vim.cmd("silent! nunmap ]xx")
-			vim.cmd("silent! xunmap [y")
-			vim.cmd("silent! xunmap ]y")
-			vim.cmd("silent! nunmap [yy")
-			vim.cmd("silent! nunmap ]yy")
-			-- remove keymap for nvim-treesitter-textobjects
-			vim.cmd("silent! nunmap [o")
-			vim.cmd("silent! nunmap ]o")
-			vim.cmd("silent! nunmap [o<Esc>")
-			vim.cmd("silent! nunmap ]o<Esc>")
 		end,
-	},
-	{
-		"nvim-mini/mini.bracketed",
-		version = "*",
-		keys = {
-			{ "[k" },
-			{ "]k" },
-			{ "[x" },
-			{ "]x" },
-			{ "[i" },
-			{ "]i" },
-			{ "[j" },
-			{ "]j" },
-			{ "[y" },
-			{ "]y" },
-		},
-		opts = {
-			buffer = { suffix = "", options = {} },
-			comment = { suffix = "k", options = {} },
-			conflict = { suffix = "x", options = {} },
-			diagnostic = { suffix = "", options = {} },
-			file = { suffix = "", options = {} },
-			indent = { suffix = "i", options = { change_type = "diff" } },
-			jump = { suffix = "j", options = {} },
-			location = { suffix = "", options = {} },
-			oldfile = { suffix = "", options = {} },
-			quickfix = { suffix = "", options = {} },
-			treesitter = { suffix = "", options = {} },
-			undo = { suffix = "", options = {} },
-			window = { suffix = "", options = {} },
-			yank = { suffix = "y", options = {} },
-		},
 	},
 	{
 		"tpope/vim-rsi",
