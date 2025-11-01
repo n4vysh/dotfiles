ce3714ae011b4a966497c36f0faf86d61cade1f7
n4vysh <n4vysh@n4vysh.dev>
2025-11-01 22:20:49 +0900

    refactor(nvim): :recycle: replace eager loading with lazy key mappings for leap.nvim

--- home/dot_config/nvim/lua/user/plugins/motion.lua
+++ home/dot_config/nvim/lua/user/plugins/motion.lua
@@ -1,11 +1,9 @@
 return {
 	{
 		"ggandor/leap.nvim",
-		-- NOTE: leap.nvim lazy load automatically
-		-- https://github.com/ggandor/leap.nvim#lazy-loading
-		lazy = false,
-		config = function()
-			require("leap").set_default_mappings()
-		end,
+		keys = {
+			{ "s", "<Plug>(leap)", mode = { "n", "x", "o" } },
+			{ "S", "<Plug>(leap-from-window)" },
+		},
 	},
 }
