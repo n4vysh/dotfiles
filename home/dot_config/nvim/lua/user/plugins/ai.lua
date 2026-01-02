9e3641c4c794742b206d0cd831bb1920f8d33244
n4vysh <n4vysh@n4vysh.dev>
2026-01-03 00:12:15 +0900

    feat(nvim): :sparkles: change model name of avante.nvim

--- home/dot_config/nvim/lua/user/plugins/ai.lua
+++ home/dot_config/nvim/lua/user/plugins/ai.lua
@@ -126,7 +126,7 @@ return {
 					},
 					openai = {
 						endpoint = "http://127.0.0.1:4000/v1",
-						model = "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
+						model = "bedrock/converse/us.anthropic.claude-sonnet-4-5-20250929-v1:0",
 						-- NOTE: use the same max_tokens value as litellm's default
 						-- https://github.com/BerriAI/litellm/blob/05e0a6d8d5a52e5cacebebe8e9ba37b3a9c800ef/litellm/constants.py
 						extra_request_body = {
