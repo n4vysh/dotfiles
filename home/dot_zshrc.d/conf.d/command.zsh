c256930a64d9fb5bd0a26ec68ba1addae5a01cd2
n4vysh <n4vysh@n4vysh.dev>
2026-02-13 03:36:39 +0900

    feat(wt): :sparkles: add config

--- home/dot_zshrc.d/conf.d/command.zsh
+++ home/dot_zshrc.d/conf.d/command.zsh
@@ -1,4 +1,5 @@
 eval "$(zoxide init zsh)"
+eval "$(command wt config shell init zsh)"
 
 # NOTE: suppress output after instant prompt
 # https://github.com/romkatv/powerlevel10k#how-do-i-initialize-direnv-when-using-instant-prompt
