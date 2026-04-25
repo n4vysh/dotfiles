9a905470a3252053c4f9920a71c7d52fdfc56688
n4vysh <n4vysh@n4vysh.dev>
2026-04-26 05:35:19 +0900

    feat(nvim): :sparkles: add clever-f.vim

--- home/dot_config/nvim/lua/user/plugins/motion.lua
+++ home/dot_config/nvim/lua/user/plugins/motion.lua
@@ -1,5 +1,56 @@
 return {
 	{
+		"rhysd/clever-f.vim",
+		init = function()
+			vim.g.clever_f_across_no_line = 1
+			vim.g.clever_f_not_overwrites_standard_mappings = 1
+		end,
+		keys = {
+			{
+				"f",
+				"<Plug>(clever-f-f)",
+				mode = { "n", "x", "o" },
+				remap = true,
+			},
+			{
+				"F",
+				"<Plug>(clever-f-F)",
+				mode = { "n", "x", "o" },
+				remap = true,
+			},
+			{
+				"t",
+				"<Plug>(clever-f-t)",
+				mode = { "n", "x", "o" },
+				remap = true,
+			},
+			{
+				"T",
+				"<Plug>(clever-f-T)",
+				mode = { "n", "x", "o" },
+				remap = true,
+			},
+			{
+				";",
+				"<Plug>(clever-f-repeat-forward)",
+				mode = { "n", "x", "s", "o" },
+				remap = true,
+			},
+			{
+				",",
+				"<Plug>(clever-f-repeat-back)",
+				mode = { "n", "x", "s", "o" },
+				remap = true,
+			},
+			{
+				"<Esc>",
+				"<Plug>(clever-f-reset)",
+				mode = { "n" },
+				remap = true,
+			},
+		},
+	},
+	{
 		url = "https://codeberg.org/andyg/leap.nvim",
 		keys = {
 			{ "s", "<Plug>(leap)", mode = { "n", "x", "o" } },
