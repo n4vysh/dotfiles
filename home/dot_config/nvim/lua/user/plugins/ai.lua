d4dbf5aef2f7e080e1879fc538d69935791307fa
n4vysh <n4vysh@n4vysh.dev>
2026-08-10 12:49:57 +0900

    feat(nvim): :sparkles: bump plugins

--- home/dot_config/nvim/lua/user/plugins/ai.lua
+++ home/dot_config/nvim/lua/user/plugins/ai.lua
@@ -106,9 +106,22 @@ return {
 					input = {}, -- Enhances `ask()`
 					picker = { -- Enhances `select()`
 						actions = {
-							opencode_send = function(...)
-								return require("opencode").snacks_picker_send(
-									...
+							opencode_send = function(picker) ---@param picker snacks.Picker
+								local items = vim.tbl_map(
+									function(item) ---@param item snacks.picker.Item
+										return item.file
+												and require("opencode").format({
+													path = item.file,
+													from = item.pos,
+													to = item.end_pos,
+												})
+											or item.text
+									end,
+									picker:selected({ fallback = true })
+								)
+
+								require("opencode").prompt(
+									table.concat(items, ", ") .. " "
 								)
 							end,
 						},
